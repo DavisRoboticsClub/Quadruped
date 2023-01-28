ae55cd70a88488865ac9132d6f1686aae6dfc892
TimHenkins <43077776+TimHenkins@users.noreply.github.com>
2023-01-27 18:40:57 -0800

    Update example_servo_code.ino
    Took code from discord

--- example_servo_code.ino
+++ example_servo_code.ino
@@ -1,15 +1,22 @@
-#include <Servo.h>   
-Servo testServo; 
-void setup() {
-  testServo.attach(8);
-  
+#include <Servo.h>
 
+Servo FLBR; 
+Servo FRBL;
+
+
+void setup() {
+  FLBR.attach(9);  // attaches the servo on pin 9 to the servo object
+  FRBL.attach(10);
+  FLBR.write(90);              // tell servo to go to position in variable 'pos'
+  FRBL.write(90);              // tell servo to go to position in variable 'pos'
+  delay(3000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-testServo.write(0);
-delay(1000);
-testServo.write(180);
-delay(1000);
+    delay(1000);
+    FLBR.write(135);              // tell servo to go to position in variable 'pos'
+    FRBL.write(135);              // tell servo to go to position in variable 'pos'
+    delay(1000);
+    FLBR.write(45);              // tell servo to go to position in variable 'pos'
+    FRBL.write(45);              // tell servo to go to position in variable 'pos'
 }
